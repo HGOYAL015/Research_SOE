888a7092622ddbfdf68e25287d60f89a190d574e
hgoyal015 <hgoyal015@gmail.com>
2020-03-16 20:15:19 +0530

    all things merged

--- Topic/ngram_topic.sh
+++ Topic/ngram_topic.sh
@@ -4,4 +4,4 @@ python3 stam_let.py < input.txt > stammed_input.txt
 
 python3 word.py > ngram.txt
 echo "ngram.txt" > input.txt
-python3 ../BXB.py < input.txt > B-Tgram-B.csv
+python3 ../BXB.py < input.txt > B-Tgram-B_ans.csv

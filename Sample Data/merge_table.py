229c6d29430d688fded6ab44dda83ff76eca0da9
hgoyal015 <hgoyal015@gmail.com>
2020-03-16 20:04:51 +0530

    Merging all metapaths

--- Sample Data/merge_table.py
+++ Sample Data/merge_table.py
@@ -6,7 +6,7 @@ def check(file,i):
     for line in fo:
         # print(line)
         a,b,c=line.split('~')
-        a=a+" "+b
+        a=a+","+b
         c=c[:-1]
         if a in dekh.keys():
             dekh[a][i]=c
@@ -26,16 +26,16 @@ for line in file_handle:
     check(text,i)
     i+=1
 
-print("source dest",end="")
+print("source,dest",end="")
 file_handle.seek(0, 0)
 for line in file_handle:
     line=line[:-1]
-    print(" "+line,end="")
+    print(","+line,end="")
 print()
 for i in dekh:
     print(i,end="")
     for j in dekh[i]:
-        print(" "+j,end="")
+        print(","+j,end="")
     print()
         
 
